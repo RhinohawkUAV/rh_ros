29ae2b23fb77ce0a8b9cad311a56eb3ab31c3db7
ubuntu <ubuntu@tegra-rhinohawk>
2000-01-01 00:12:50 +0000

    Make cuda work

--- depth_vision/src/depth_vision.cpp
+++ depth_vision/src/depth_vision.cpp
@@ -6,6 +6,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+void  edgedetect(unsigned char * pix, int count, int cols);
 
 bool process;
 int frameInt;
@@ -25,20 +26,23 @@ using namespace cv;
   int main(int argc, char ** argv){
 
   ros::init(argc, argv, "dv_vision");
-
-
-  VideoCapture vcap(0);
-
-  Mat frame;
+//VideoCapture vcap;  
+//vcap.open("http://192.168.10.170:8081");
+VideoCapture vcap("/home/ubuntu/CVTestNova.mp4");
+vcap.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
+vcap.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
+  Mat frame2;
+  Mat frame;\
+  namedWindow("frame", CV_WINDOW_AUTOSIZE);
   namedWindow("Depth_Vision", CV_WINDOW_AUTOSIZE);
   startWindowThread();
-
+  
   ros::NodeHandle n;
 
   ros::Subscriber dv_sub = n.subscribe("masterControl", 1000, commandCallback);
 
   ros::Rate loop_rate(30);
-
+  frame = imread("/run/user/1000/gvfs/gphoto2:host=%5Busb%3A002%2C057%5D/DCIM/102D5100/DSC_0077.JPG");
   while (ros::ok()){
 
     //opemcv code
@@ -51,7 +55,13 @@ using namespace cv;
       std::cout << "noframe!";
     }
     else{
-       imshow("Depth_Vision", frame);
+	resize(frame, frame, Size(960,540));
+	cvtColor(frame, frame2, CV_RGB2GRAY);
+       unsigned char * data = (unsigned char *)(frame2.data);
+       edgedetect( data, frame2.rows * frame2.cols, frame2.cols);
+      // frame2.data = data;
+       imshow("Depth_Vision", frame2);
+       imshow("frame", frame);
 }
     //message part
     ros::spinOnce();
