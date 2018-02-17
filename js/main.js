e24794de73d084bcbe0f704339e75425e44121ce
Molly Johnson <itsmollyj@gmail.com>
2018-02-17 17:18:59 -0500

    Added guages to speed, added toggle to auto center map

--- js/main.js
+++ js/main.js
@@ -1,20 +1,31 @@
 
 var ros;
-var mapInit = false;
 var clockInit = false;
+var centerMap = true;
 
 var uavPath=[];
 
-var startTime;
-var lastTime;
-var currentTime;
-var lastLat;
-var lastLong;
-var currentLat;
-var currentLong;
+// Guage Settigns -----------------------------------------------------------------------
+var opts = {
+  angle: -0.26, // The span of the gauge arc
+  lineWidth: 0.07, // The line thickness
+  radiusScale: .60, // Relative radius
+  pointer: {
+    length: 0, // // Relative to gauge radius
+    strokeWidth: 0, // The thickness
+    color: '#000000' // Fill color
+  },
+  limitMax: false,     // If false, max value increases automatically if value > maxValue
+  limitMin: true,     // If true, the min value of the gauge will be fixed
+  colorStart: '#fff',   // Colors
+  colorStop: '#fff',    // just experiment with them
+  strokeColor: 'rgba(255, 255, 255, .3)',  // to see which ones work best for you
+  generateGradient: false,
+  highDpiSupport: true,     // High resolution support
+  
+};
 
-var theData = {"type": "Feature", "geometry": {"type": "LineString", "coordinates": []}};
-
+var guageTopSpeed = 20;
 
 
 // Connect to ROSBridge -----------------------------------------------------------------------
@@ -53,16 +64,16 @@ function connectToTopics() {
       messageType: 'sensor_msgs/NavSatFix'
     });
 
+  var groundSpeedTopic = new ROSLIB.Topic({
+      ros: ros,
+      name: '/mavros/global_position/raw/gps_vel',
+      messageType: 'geometry_msgs/TwistStamped'
+    });
+
    var imageTopic = new ROSLIB.Topic({
       ros : ros,
       name : '/gopro/aruco_marker/compressed',
       messageType : 'sensor_msgs/CompressedImage'
-    });
-
-   var markerLocation = new ROSLIB.Topic({
-      ros: ros,
-      name: '/gopro/landing_box_location',
-      messageType: 'geometry_msgs/PointStamped'
     });
 
   var clockTopic = new ROSLIB.Topic({
@@ -70,15 +81,6 @@ function connectToTopics() {
       name : '/mavros/time_reference',
       messageType : 'sensor_msgs/TimeReference'
     });
-
-  markerLocation.subscribe(function(message) {
-    console.log("Landing Box Location: x: "+message.point.x +", y: "+message.point.y +", z: "+message.point.z);
-
-    var newDot = document.createElement('div');
-    newDot.setAttribute('class', 'landing-box-loc');
-    newDot.setAttribute('style', 'left:'+message.point.x/2+'px; top:'+message.point.y/2+'px;');
-    document.getElementById('graph-scatter-plot').appendChild(newDot);
-  });
 
   navSatTopic.subscribe(function(message) {
 
@@ -92,8 +94,6 @@ function connectToTopics() {
 
     document.getElementById("tel-lat").innerHTML= message.latitude;
     document.getElementById("tel-long").innerHTML= message.longitude;
-
-    
   });
 
   compassTopic.subscribe(function(message) {
@@ -106,77 +106,50 @@ function connectToTopics() {
     document.getElementById( 'aruco-image' ).setAttribute( 'src', img );
   });
 
-
   altitudeTopic.subscribe(function(message) {
     document.getElementById("tel-altitude").innerHTML= Math.round(message.data*3.28084) + "<span> FT</span>";
   });
 
-  clockTopic.subscribe(function(message){
-    var theDate = new Date();
-    theDate.setTime(message.time_ref.secs*1000);
-    //console.log(theDate.getSeconds());
-    
-    document.getElementById("tel-time").innerHTML = theDate.getHours()+":"+theDate.getMinutes()+":"+theDate.getSeconds();
+  groundSpeedTopic.subscribe(function(message) {
+    document.getElementById("tel-speed").innerHTML= getGroundVelocity(message.twist).toFixed(1) + "<span> M/S</span>";
+    groundSpeed.set(getGroundVelocity(message.twist));
 
-
-/*
-    if(clockInit == false){
-      startTime = theDate;
-      lastTime = theDate;
-      currentTime = message.time_ref.secs;
-      clockInit = true;
-      currentLong = uavPath[uavPath.length-1][0];
-      currentLat = uavPath[uavPath.length-1][1];
-    }else{
-      if(message.time_ref.secs != currentTime){
-        lastLong = currentLong;
-        lastLat = currentLat;
-        currentLong = uavPath[uavPath.length-1][0];
-        currentCat = uavPath[uavPath.length-1][1];
-
-        lastTime = currentTime;
-        currentTime = message.time_ref.secs;
-
-        var deltaTime = (currentTime - lastTime);
-
-        document.getElementById("tel-speed").innerHTML = calcSpeed(lastLat, lastLong, currentLat, currentLong, deltaTime).toFixed(2) + "<span>M/S</span>";
-      }  
-    }*/
+    /* ********* if using VFR_HUD
+    document.getElementById("tel-speed").innerHTML= message.groundspeed.toFixed(1) + "<span> M/S</span>";
+    document.getElementById("tel-airSpeed").innerHTML= message.airspeed.toFixed(1) + "<span> M/S</span>";
+    groundSpeed.set(message.groundspeed);
+    airSpeed.set(message.airspeed);*/
   });
 
+var groundSpeed = new Gauge( document.getElementById('groundGuage')).setOptions(opts); // create sexy gauge!
+var airSpeed = new Gauge(document.getElementById('airGuage')).setOptions(opts); // create sexy gauge!
+
+groundSpeed.maxValue =guageTopSpeed; // set max gauge value
+airSpeed.maxValue =guageTopSpeed; // set max gauge value
 }
+
+
+function getGroundVelocity(data){
+  var velocity = Math.sqrt(Math.abs(Math.pow(data.linear.x, 2)+Math.pow(data.linear.y, 2)));
+  return velocity;
+}
+
+
+  
 
 // update path on map---------------------------------------------
 
 function updateMapPath(){
   var newLine = [uavPath[uavPath.length-2],uavPath[uavPath.length-1]]
   var polyline = L.polyline(newLine, {color: '#4ABDE2'}).addTo(map);
-}
-
-
-
-function calcSpeed (lastLat, lastLong, currentLat, currentLong, time){
-  var kilometers = calcDistance(lastLat, lastLong, currentLat, currentLong, "K");
   
-  var metersPerSecond = (kilometers*1000)/time;
-
-  return metersPerSecond;
+  if(centerMap){
+    map.setView(uavPath[uavPath.length-1]);
+  }
 }
 
 
-function calcDistance(lat1, lon1, lat2, lon2, unit) {
-  var radlat1 = Math.PI * lat1/180;
-  var radlat2 = Math.PI * lat2/180;
-  var theta = lon1-lon2;
-  var radtheta = Math.PI * theta/180;
-  var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-  dist = Math.acos(dist);
-  dist = dist * 180/Math.PI;
-  dist = dist * 60 * 1.1515;
-  if (unit=="K") { dist = dist * 1.609344 };
-  if (unit=="N") { dist = dist * 0.8684 };
-  return dist;
-}
+
 
 function getDirection(rotation){
   var direction; 
@@ -201,3 +174,64 @@ function getDirection(rotation){
 
   return direction;
 }
+
+function toggleMapCenter(toToggle){
+  if(centerMap){
+    centerMap = false;
+    document.getElementById(toToggle).setAttribute('class', 'toggleOff');
+  }else{
+    centerMap = true;
+     document.getElementById(toToggle).setAttribute('class', 'toggleOn');
+  }
+}
+
+
+
+ /* ************************************************      MARKER LOCATION
+ var markerLocation = new ROSLIB.Topic({
+      ros: ros,
+      name: '/gopro/landing_box_location',
+      messageType: 'geometry_msgs/PointStamped'
+    });
+ markerLocation.subscribe(function(message) {
+    console.log("Landing Box Location: x: "+message.point.x +", y: "+message.point.y +", z: "+message.point.z);
+
+    var newDot = document.createElement('div');
+    newDot.setAttribute('class', 'landing-box-loc');
+    newDot.setAttribute('style', 'left:'+message.point.x/2+'px; top:'+message.point.y/2+'px;');
+    document.getElementById('graph-scatter-plot').appendChild(newDot);
+  });
+*/
+/* *************************************************      SET UP CLOCK
+clockTopic.subscribe(function(message){
+    var theDate = new Date();
+    theDate.setTime(message.time_ref.secs*1000);
+    //console.log(theDate.getSeconds());
+    
+    document.getElementById("tel-time").innerHTML = theDate.getHours()+":"+theDate.getMinutes()+":"+theDate.getSeconds();
+
+
+    if(clockInit == false){
+      startTime = theDate;
+      lastTime = theDate;
+      currentTime = message.time_ref.secs;
+      clockInit = true;
+      currentLong = uavPath[uavPath.length-1][0];
+      currentLat = uavPath[uavPath.length-1][1];
+    }else{
+      if(message.time_ref.secs != currentTime){
+        lastLong = currentLong;
+        lastLat = currentLat;
+        currentLong = uavPath[uavPath.length-1][0];
+        currentCat = uavPath[uavPath.length-1][1];
+
+        lastTime = currentTime;
+        currentTime = message.time_ref.secs;
+
+        var deltaTime = (currentTime - lastTime);
+
+        document.getElementById("tel-speed").innerHTML = calcSpeed(lastLat, lastLong, currentLat, currentLong, deltaTime).toFixed(2) + "<span>M/S</span>";
+      }  
+    }*/
+
+
