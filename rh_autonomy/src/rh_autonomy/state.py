4b69c8e4566151c98290fc85472bbb9c7a8363aa
Konrad Rokicki <krokicki@gmail.com>
2018-07-06 22:55:00 -0400

    WIP for state

--- rh_autonomy/src/rh_autonomy/state.py
+++ rh_autonomy/src/rh_autonomy/state.py
@@ -1,36 +1,7 @@
 #!/usr/bin/env python
 """
-ROS node implementing Rhinohawk global mission state, including:
-
-- mission definition
-    - geofence  (polygon)
-    - static no fly zones
-    - roads
-    - base location
-    - transit waypoints
-    - joe's location
-    
-- dynamic no fly zones
-
-- current GPS location
-
-- current mission phase 
-    BASE
-    TAKE_OFF
-    OUTBOUND
-    SEARCH
-    REMOTE_LAND
-    REMOTE_WAIT
-    REMOTE_TAKE_OFF
-    INBOUND
-    LAND
-
-- navigation waypoints (with mapping to mission phases, and transit waypoints)
-
-- purported marker locations
-
-- chosen landing marker location
-
+ROS node implementing Rhinohawk global mission state. See rh_msgs.msg.State
+for a full description.
 """
 
 from functools import partial
@@ -40,7 +11,7 @@ import rospy
 from sensor_msgs.msg import NavSatFix
 from mavros_msgs.msg import WaypointList, WaypointReached, StatusText
 
-from rh_msgs.msg import State
+from rh_msgs.msg import State, Mission, GPSCoord
 from rh_msgs.srv import GetState, GetStateResponse
 from rh_msgs.srv import SetMission, SetMissionResponse
 from rh_msgs.srv import SetNoFlyZones, SetNoFlyZonesResponse
@@ -68,7 +39,11 @@ class StateNode():
 
     def __init__(self):
         self.values = LatchMap()
-        self.mission = None
+        self.mission = Mission()
+        self.dynamic_nfzs = []
+        self.apm_wps = []
+        self.target_mission_wp = 0
+        self.landing_location = GPSCoord()
         self.reached_wp_index = -1
 
         rospy.init_node("state")
@@ -90,20 +65,20 @@ class StateNode():
             log("We have reached waypoint %s" % msg.wp_seq)
             self.reached_wp_index = msg.wp_seq
 
-            if not self.apm_waypoints:
+            if not self.apm_wps:
                 warn("Waypoint reached, but no APM waypoints")
                 return
 
             try:
-                waypoint = self.apm_waypoints[msg.wp_seq]
+                waypoint = self.apm_wps[msg.wp_seq]
 
             except:
                 warn("Waypoint reached, but not in APM waypoints")
 
 
     def waypoints_changed(self, msg):
-        self.apm_waypoints = msg.waypoints
-        log("Waypoints list has changed (%d waypoints)" % len(self.apm_waypoints))
+        self.apm_wps = msg.waypoints
+        log("Waypoints list has changed (%d waypoints)" % len(self.apm_wps))
 
 
     def mavlink_statustext(self, msg):
@@ -130,8 +105,11 @@ class StateNode():
 
         state = State()
         state.mission = self.mission
+        state.dynamic_nfzs = self.dynamic_nfzs
+        state.target_mission_wp = self.target_mission_wp
         state.gps_position = self.values.get_value(gps_topic)
-
+        state.apm_wps = self.apm_wps
+        state.landing_location = self.landing_location
         return GetStateResponse(state)
 
 
