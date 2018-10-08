12bbb4a65682e00a6053ceca1898d923376d9f84
shp <stephenpheiffer1@gmail.com>
2018-10-08 17:37:44 +0300

    Updated default maximum path time.

--- pathFinding/constants.py
+++ pathFinding/constants.py
@@ -22,7 +22,7 @@ DEFAULT_WAYPOINT_ACCEPTANCE_RADII = 120.0
 DEFAULT_NFZ_BUFFER_WIDTH = 25.0
 DEFAULT_NFZ_TARGET_OFFSET = 50.0
 DEFAULT_VERTEX_HEURISTIC_MULTIPLIER = 1.25
-DEFAULT_MAXIMUM_TIME = 2000.0
+DEFAULT_MAXIMUM_TIME = 5000.0
 
 # When making a buffer around an NFZ the buffer's edges will be a configurable width away from the original buffer.
 # Vertices will be further away to create the desired width and very sharp vertice may be pushed out a lot.  This is maximum
