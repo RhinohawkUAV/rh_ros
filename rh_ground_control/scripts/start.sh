d46837db14a8b0c1952f39b1cc873a5e21659ea2
Konrad Rokicki <krokicki@gmail.com>
2018-11-28 19:30:45 -0500

    renamed visionTesting to index.html

--- rh_ground_control/scripts/start.sh
+++ rh_ground_control/scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Path should be parameterized
-xdg-open $HOME/catkin_ws/src/rh_ros/rh_ground_control/web/visionTesting.html
+xdg-open $HOME/catkin_ws/src/rh_ros/rh_ground_control/web/index.html
 
